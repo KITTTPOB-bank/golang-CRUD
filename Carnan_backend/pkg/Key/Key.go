344c1eedb34c4b836f916a4615200376f76274ae
KITTTPOB-bank <160011796+KITTTPOB-bank@users.noreply.github.com>
2024-04-24 14:26:55 +0700

    Add files via upload

---
+++ Carnan_backend/pkg/Key/Key.go
@@ -0,0 +1,12 @@
+package Key
+
+func GetIDAWSs3() (string, string) {
+
+	aws_access_key_id := "****"
+	aws_secret_access_key := "****"
+	return aws_access_key_id, aws_secret_access_key
+}
+
+func GetMongoDB() string {
+	return "****"
+}
